62ce8af84a4a7efb41e004868f725e79be9b2f34
BePPPower <ftw2139@163.com>
2023-11-30 22:15:20 +0800

    support select db below hive 3

--- fe/fe-core/src/main/java/org/apache/doris/datasource/hive/JdbcPostgreSQLClientCachedClient.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/hive/JdbcPostgreSQLClientCachedClient.java
@@ -36,6 +36,8 @@ import org.apache.hadoop.hive.metastore.api.LockResponse;
 import org.apache.hadoop.hive.metastore.api.NotificationEventResponse;
 import org.apache.hadoop.hive.metastore.api.Partition;
 import org.apache.hadoop.hive.metastore.api.PrincipalType;
+import org.apache.hadoop.hive.metastore.api.SerDeInfo;
+import org.apache.hadoop.hive.metastore.api.SerdeType;
 import org.apache.hadoop.hive.metastore.api.StorageDescriptor;
 import org.apache.hadoop.hive.metastore.api.Table;
 import org.apache.hadoop.hive.metastore.api.TableValidWriteIds;
@@ -191,9 +193,9 @@ public class JdbcPostgreSQLClientCachedClient extends JdbcClientCachedClient {
 
     @Override
     public Table getTable(String dbName, String tblName) throws Exception {
-        String sql = "SELECT \"TBL_NAME\", \"DBS\".\"NAME\", \"OWNER\", \"CREATE_TIME\","
+        String sql = "SELECT \"TBL_ID\", \"TBL_NAME\", \"DBS\".\"NAME\", \"OWNER\", \"CREATE_TIME\","
                 + " \"LAST_ACCESS_TIME\", \"LAST_ACCESS_TIME\", \"RETENTION\","
-                + " \"IS_REWRITE_ENABLED\", \"VIEW_EXPANDED_TEXT\", \"VIEW_ORIGINAL_TEXT\", \"OWNER_TYPE\""
+                + " \"IS_REWRITE_ENABLED\", \"VIEW_EXPANDED_TEXT\", \"VIEW_ORIGINAL_TEXT\", \"DBS\".\"OWNER_TYPE\""
                 + " FROM \"TBLS\" join \"DBS\" on \"TBLS\".\"DB_ID\" = \"DBS\".\"DB_ID\" "
                 + " WHERE \"DBS\".\"NAME\" = '" + dbName + "' AND \"TBLS\".\"TBL_NAME\"='" + tblName + "';";
         LOG.debug("getTable exec sql: {}", sql);
@@ -215,8 +217,8 @@ public class JdbcPostgreSQLClientCachedClient extends JdbcClientCachedClient {
                 hiveTable.setViewOriginalText(rs.getString("VIEW_ORIGINAL_TEXT"));
 
                 hiveTable.setSd(getStorageDescriptor(dbName, tblName));
-                hiveTable.setParameters(getParameters(dbName, tblName));
-                hiveTable.setPartitionKeys(getTablePartitionKeys(dbName, tblName));
+                hiveTable.setParameters(getTableParameters(rs.getInt("TBL_ID")));
+                hiveTable.setPartitionKeys(getTablePartitionKeys(rs.getInt("TBL_ID")));
                 return hiveTable;
             }
             throw new Exception("Can not get Table from PG databases. dbName = " + dbName + ", tblName = " + tblName);
@@ -241,6 +243,7 @@ public class JdbcPostgreSQLClientCachedClient extends JdbcClientCachedClient {
                 sd.setNumBuckets(rs.getInt("NUM_BUCKETS"));
                 sd.setOutputFormat(rs.getString("OUTPUT_FORMAT"));
                 sd.setStoredAsSubDirectories(rs.getBoolean("IS_STOREDASSUBDIRECTORIES"));
+                sd.setSerdeInfo(getSerdeInfo(rs.getInt("SERDE_ID")));
                 return sd;
             }
             throw new Exception("Can not get StorageDescriptor from PG databases of " + dbName + "." + tblName);
@@ -262,18 +265,52 @@ public class JdbcPostgreSQLClientCachedClient extends JdbcClientCachedClient {
                 sd.setNumBuckets(rs.getInt("NUM_BUCKETS"));
                 sd.setOutputFormat(rs.getString("OUTPUT_FORMAT"));
                 sd.setStoredAsSubDirectories(rs.getBoolean("IS_STOREDASSUBDIRECTORIES"));
-                close(rs);
+                sd.setSerdeInfo(getSerdeInfo(rs.getInt("SERDE_ID")));
                 return sd;
             }
             throw new Exception("Can not get StorageDescriptor from PG databases, SD_ID = " + sdId + ".");
         }
     }
 
-    private List<FieldSchema> getTablePartitionKeys(String dbName, String tblName) throws Exception {
-        String sql = "SELECT \"PKEY_NAME\", \"PKEY_TYPE\", \"PKEY_COMMENT\" from \"PARTITION_KEYS\" WHERE \"TBL_ID\"=("
-                + "SELECT \"TBL_ID\" FROM \"TBLS\" join \"DBS\" on \"TBLS\".\"DB_ID\" = \"DBS\".\"DB_ID\" "
-                + "WHERE \"DBS\".\"NAME\" = '" + dbName + "' AND \"TBLS\".\"TBL_NAME\"='" + tblName + "'"
-                + ");";
+    private SerDeInfo getSerdeInfo(int serdeId) throws Exception {
+        String sql = "SELECT * FROM \"SERDES\" WHERE \"SERDE_ID\" = " + serdeId;
+        LOG.debug("getSerdeInfo exec sql: {}", sql);
+
+        SerDeInfo serDeInfo = new SerDeInfo();
+        serDeInfo.setParameters(getSerdeInfoParameters(serdeId));
+
+        try (Connection conn = getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery()) {
+            if (rs.next()) {
+                serDeInfo.setName(rs.getString("NAME"));
+                serDeInfo.setSerializationLib(rs.getString("SLIB"));
+                return serDeInfo;
+            }
+            throw new Exception("Can not get SerDeInfo from PG databases, serdeId = " + serdeId + ".");
+        }
+
+
+    }
+
+    private Map<String, String> getSerdeInfoParameters(int serdeId) throws Exception {
+        String sql = "SELECT \"PARAM_KEY\", \"PARAM_VALUE\" from \"SERDE_PARAMS\" WHERE \"SERDE_ID\" = " + serdeId;
+        LOG.debug("getSerdeInfoParameters exec sql: {}", sql);
+
+        try (Connection conn = getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery()) {
+            ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
+            while (rs.next()) {
+                builder.put(rs.getString("PARAM_KEY"), rs.getString("PARAM_VALUE"));
+            }
+            return builder.build();
+        }
+    }
+
+    private List<FieldSchema> getTablePartitionKeys(int tableId) throws Exception {
+        String sql = "SELECT \"PKEY_NAME\", \"PKEY_TYPE\", \"PKEY_COMMENT\" from \"PARTITION_KEYS\""
+                + " WHERE \"TBL_ID\"= " + tableId;
         LOG.debug("getTablePartitionKeys exec sql: {}", sql);
 
         try (Connection conn = getConnection();
@@ -294,11 +331,8 @@ public class JdbcPostgreSQLClientCachedClient extends JdbcClientCachedClient {
         }
     }
 
-    private Map<String, String> getParameters(String dbName, String tblName) throws Exception {
-        String sql = "SELECT \"PARAM_KEY\", \"PARAM_VALUE\" from \"TABLE_PARAMS\" WHERE \"TBL_ID\" = ("
-                + "SELECT \"TBL_ID\" FROM \"TBLS\" join \"DBS\" on \"TBLS\".\"DB_ID\" = \"DBS\".\"DB_ID\""
-                + " WHERE \"DBS\".\"NAME\" = '" + dbName + "' AND \"TBLS\".\"TBL_NAME\"='" + tblName + "'"
-                + ");";
+    private Map<String, String> getTableParameters(int tableId) throws Exception {
+        String sql = "SELECT \"PARAM_KEY\", \"PARAM_VALUE\" from \"TABLE_PARAMS\" WHERE \"TBL_ID\" = " + tableId;
         LOG.debug("getParameters exec sql: {}", sql);
 
         try (Connection conn = getConnection();
@@ -325,9 +359,20 @@ public class JdbcPostgreSQLClientCachedClient extends JdbcClientCachedClient {
         }
     }
 
+    private SerdeType getSerdeType(String serdeTypeString) throws Exception {
+        switch (serdeTypeString) {
+            case "HIVE":
+                return SerdeType.findByValue(1);
+            case "SCHEMA_REGISTRY":
+                return SerdeType.findByValue(2);
+            default:
+                throw new Exception("Unknown serde type of this table");
+        }
+    }
+
     @Override
     public List<FieldSchema> getSchema(String dbName, String tblName) throws Exception {
-        String sql = "SELECT \"COLUMN_NAME\", \"TYPE_NAME\", \"COMMENT\""
+        String sql = "SELECT \"COLUMN_NAME\", \"TYPE_NAME\", \"COMMENT\", \"TBLS\".\"TBL_ID\""
                 + " FROM \"TBLS\" join \"DBS\" on \"TBLS\".\"DB_ID\" = \"DBS\".\"DB_ID\""
                 + " join \"SDS\" on \"SDS\".\"SD_ID\" = \"TBLS\".\"SD_ID\""
                 + " join \"COLUMNS_V2\" on \"COLUMNS_V2\".\"CD_ID\" = \"SDS\".\"CD_ID\""
@@ -335,6 +380,7 @@ public class JdbcPostgreSQLClientCachedClient extends JdbcClientCachedClient {
         LOG.debug("getSchema exec sql: {}", sql);
 
         Builder<FieldSchema> builder = ImmutableList.builder();
+        int tableId = -1;
         try (Connection conn = getConnection();
                 PreparedStatement stmt = conn.prepareStatement(sql);
                 ResultSet rs = stmt.executeQuery()) {
@@ -342,11 +388,15 @@ public class JdbcPostgreSQLClientCachedClient extends JdbcClientCachedClient {
                 FieldSchema fieldSchema = new FieldSchema(rs.getString("COLUMN_NAME"),
                         rs.getString("TYPE_NAME"), rs.getString("COMMENT"));
                 builder.add(fieldSchema);
+                // actually, all resultSets have the same TBL_ID.
+                tableId = rs.getInt("TBL_ID");
             }
+        } catch (Exception e) {
+            throw new Exception("Can not get schema of db = " + dbName + ", table = " + tblName);
         }
 
         // add partition columns
-        getTablePartitionKeys(dbName, tblName).stream().forEach(field -> builder.add(field));
+        getTablePartitionKeys(tableId).stream().forEach(field -> builder.add(field));
         return builder.build();
     }
 
